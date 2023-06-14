9e182522fbc95eac749315bbaff0b72438886334
ukyon <kyoyannnnn@gmail.com>
2023-06-15 00:45:35 +0900

    first commit

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -13,6 +13,8 @@ function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    //return ({first_name,family_name}) => ({["full_name"]:family_name+" "+first_name})
+    return {full_name :data["family_name"]+" "+data["first_name"]};
 }
 
 // 出力が想定される答え
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList).then()
   return await result
 }
 
-function test() {
-  return
+async function test(test) {
+  await new Promise(resolve => setTimeout(resolve, 3000));
+
+  return test.map(buildFullName);
 }
 
+function buildFullName(data) {
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  //return ({first_name,family_name}) => ({["full_name"]:family_name+" "+first_name})
+  return {full_name :data["family_name"]+" "+data["first_name"]};
+}
